55fc4760c2522aed97fde50e454d3f5f83bdfd57
ashutoshsingh298 <154498359+ashutoshsingh298@users.noreply.github.com>
2023-12-21 13:02:00 +0530

    Add files via upload

--- locations.js
+++ locations.js
@@ -1,60 +1,27 @@
-const Locations = require('../Models/locations');
+const mongoose = require('mongoose');
 
-exports.getLocations = async (req, res) => {
-    try{
-        const result = await Locations.find();
-        res.status(200).json(result);
-    }catch(err){
-        res.status(500).send(err);
+const restuarantsSchema = new mongoose.Schema({
+
+    name : {
+        type : String,
+        required : true
+    },
+    city_id : {
+        type : Number,
+        required : true
+    },
+    location_id : {
+        type : Number,
+        required : true
+    },
+    city_name : {
+        type : String,
+        required : true
+    },
+    country_name : {
+        type : String,
+        required : true
+       
     }
-};
-
-exports.createLocation = async(req,res) => {
-    const newLocation = new Locations ({
-        name : req.body.name,
-        city_id : req.body.city_id,
-        location_id : req.body.location_id,
-        country_name : req.body.country_name
-    });
-   try{
-        const a1 = await newLocation.save();
-        res.status(200).json(a1);
-   }catch(err){
-       res.status(500).send(err);
-   }
-};
-
-exports.updateLocation = async (req, res) => {
-    const location = await Locations.findById(req.params.id);
-    try{
-        if(req.body.name){
-            location.name = req.body.name;  
-        }
-        if(req.body.city_id){
-            location.city_id = req.body.city_id;
-        }
-        if(req.body.location_id){
-            location.location_id = req.body.location_id;
-        }
-        if(req.body.country_name){
-            location.country_name = req.body.country_name;
-        }
-        const a1 = await location.save()
-        res.status(200).json(a1);
-             
-    }catch(err){
-        res.status(500).send(err);
-    }
-};
-
-exports.deleteLocation = async (req, res) => {
-    const location = await Locations.findById(req.params.id);
-    try{
-        const d1 = await location.remove();
-        res.status(200).json(d1);
-    }catch(err){
-        res.status(500).send(err);
-    }
-}
-
-
+});
+module.exports = mongoose.model('locations', restuarantsSchema);
