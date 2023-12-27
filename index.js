eb40004d31f759614435a427734345459eb9b423
ashutoshsingh298 <154498359+ashutoshsingh298@users.noreply.github.com>
2023-12-27 12:14:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,162 @@
+/*!
+ * range-parser
+ * Copyright(c) 2012-2014 TJ Holowaychuk
+ * Copyright(c) 2015-2016 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
-// Import express and router
-const express = require('express');
-const routes = express.Router();
+'use strict'
 
-// Import all required controlling functions
-const restuarantsController = require('../Controllers/RestuarantsController');
-const locationsController = require('../Controllers/locations');
-const mealtypeController = require('../Controllers/mealtypes');
-const menuItemsController = require('../Controllers/MenuItemsController');
-const userController = require('../Controllers/userController');
-const paymentController = require('../Controllers/Payment');
+/**
+ * Module exports.
+ * @public
+ */
 
+module.exports = rangeParser
 
-// These are API end points for my project
-routes.get('/locations',locationsController.getLocations);
-routes.get('/mealtypes',mealtypeController.getMealtypes);
-routes.post('/filter', restuarantsController.filter);
-routes.get("/restaurants", restuarantsController.getAllRestuarant);
-routes.get('/locations/:id', restuarantsController.getRestaurantByLocation);
-routes.get('/restaurants/:id', restuarantsController.getRestaurantById);
-routes.get('/getmenu/:id', menuItemsController.getMenuItemsByRestaurant);
-routes.post('/signup', userController.signUp);
-routes.post('/login', userController.logIn);
-routes.post('/payment', paymentController.handlePayment);
+/**
+ * Parse "Range" header `str` relative to the given file `size`.
+ *
+ * @param {Number} size
+ * @param {String} str
+ * @param {Object} [options]
+ * @return {Array}
+ * @public
+ */
 
-// Exporting router.
-module.exports = routes; 
+function rangeParser (size, str, options) {
+  if (typeof str !== 'string') {
+    throw new TypeError('argument str must be a string')
+  }
+
+  var index = str.indexOf('=')
+
+  if (index === -1) {
+    return -2
+  }
+
+  // split the range string
+  var arr = str.slice(index + 1).split(',')
+  var ranges = []
+
+  // add ranges type
+  ranges.type = str.slice(0, index)
+
+  // parse all ranges
+  for (var i = 0; i < arr.length; i++) {
+    var range = arr[i].split('-')
+    var start = parseInt(range[0], 10)
+    var end = parseInt(range[1], 10)
+
+    // -nnn
+    if (isNaN(start)) {
+      start = size - end
+      end = size - 1
+    // nnn-
+    } else if (isNaN(end)) {
+      end = size - 1
+    }
+
+    // limit last-byte-pos to current length
+    if (end > size - 1) {
+      end = size - 1
+    }
+
+    // invalid or unsatisifiable
+    if (isNaN(start) || isNaN(end) || start > end || start < 0) {
+      continue
+    }
+
+    // add range
+    ranges.push({
+      start: start,
+      end: end
+    })
+  }
+
+  if (ranges.length < 1) {
+    // unsatisifiable
+    return -1
+  }
+
+  return options && options.combine
+    ? combineRanges(ranges)
+    : ranges
+}
+
+/**
+ * Combine overlapping & adjacent ranges.
+ * @private
+ */
+
+function combineRanges (ranges) {
+  var ordered = ranges.map(mapWithIndex).sort(sortByRangeStart)
+
+  for (var j = 0, i = 1; i < ordered.length; i++) {
+    var range = ordered[i]
+    var current = ordered[j]
+
+    if (range.start > current.end + 1) {
+      // next range
+      ordered[++j] = range
+    } else if (range.end > current.end) {
+      // extend range
+      current.end = range.end
+      current.index = Math.min(current.index, range.index)
+    }
+  }
+
+  // trim ordered array
+  ordered.length = j + 1
+
+  // generate combined range
+  var combined = ordered.sort(sortByRangeIndex).map(mapWithoutIndex)
+
+  // copy ranges type
+  combined.type = ranges.type
+
+  return combined
+}
+
+/**
+ * Map function to add index value to ranges.
+ * @private
+ */
+
+function mapWithIndex (range, index) {
+  return {
+    start: range.start,
+    end: range.end,
+    index: index
+  }
+}
+
+/**
+ * Map function to remove index value from ranges.
+ * @private
+ */
+
+function mapWithoutIndex (range) {
+  return {
+    start: range.start,
+    end: range.end
+  }
+}
+
+/**
+ * Sort function to sort ranges by index.
+ * @private
+ */
+
+function sortByRangeIndex (a, b) {
+  return a.index - b.index
+}
+
+/**
+ * Sort function to sort ranges by start position.
+ * @private
+ */
+
+function sortByRangeStart (a, b) {
+  return a.start - b.start
+}
